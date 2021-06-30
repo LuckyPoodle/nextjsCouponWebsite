35b1f638bbf2fe227932235922f7c3d167f27078
jui <juioftou@gmail.com>
2021-06-30 15:47:57 +0800

    server

---
+++ _static/_next/static/VMi84P0lv2mIr4-s_uHgE/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/cb1608f2-91739a39db5d417be22f.js","static/chunks/98-898b39ab49218f75a29a.js","static/chunks/pages/index-5ef4abc061ba0e9abd62.js"],"/_error":["static/chunks/pages/_error-a0e21b9b223f827fe1f2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
