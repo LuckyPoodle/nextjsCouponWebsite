afdff4f1ba12a2b446507c6704f7862a3d3f9a53
truffletou <juioftou@gmail.com>
2021-07-15 19:07:52 +0800

    added do space in config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports={
     images:{
-        domains:['https://media.publit.io','res.cloudinary.com'],
+        domains:['https://media.publit.io','res.cloudinary.com','https://promobucket.sgp1.digitaloceanspaces.com'],
         loader: "imgix",
         path: "",
     }
