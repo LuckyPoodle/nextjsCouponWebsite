50a64e051f2f6c20742f4a0472ba2b6b955edfc6
truffletou <juioftou@gmail.com>
2021-07-19 14:21:28 +0800

    added trailingslah true

--- next.config.js
+++ next.config.js
@@ -3,5 +3,6 @@ module.exports={
         domains:['https://media.publit.io','res.cloudinary.com','https://promobucket.sgp1.digitaloceanspaces.com'],
         loader: "imgix",
         path: "",
-    }
+    },
+    trailingSlash: true,
 }
