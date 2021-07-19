8725e9e93f87366610de15b160a3a00a1701a4b2
truffletou <juioftou@gmail.com>
2021-07-19 14:08:14 +0800

    testing

---
+++ pages/testing/[slug].js
@@ -0,0 +1,64 @@
+import { API_URL } from '../../config/index'
+export default function DealPage({ deal }) {
+
+  console.log("IN DEAL PAGE..")
+
+  console.log("deal...");
+  console.log(deal)
+ 
+
+  return (
+   <div className="container">
+
+       {deal?deal.Title:"hey"}
+
+   </div>
+  )
+}
+
+
+////CHANGE TO GETSTATICPROPS
+
+
+// export async function getServerSideProps(context) {
+//     const {params} =context;
+  
+//     const res = await fetch(`${API_URL}/deals?_where[urlname]=${params.specificdeal}`)
+//     const deals = await res.json()
+
+//   return {
+//     props: {
+//       deal: deals[0],
+//     },
+//   }
+// }
+
+export async function getStaticPaths() {
+  const res = await fetch(`${API_URL}/deals`)
+  const alldeals = await res.json();
+ 
+
+  const paths = alldeals.map((deal) => ({
+    params: { slug: deal.urlname },
+  }))
+
+
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export async function getStaticProps({ params: { slug } }) {
+
+    
+  
+    const res = await fetch(`${API_URL}/deals?_where[urlname]=${slug}`)
+    const deals = await res.json()
+  return {
+    props: {
+      deal: deals[0],
+    },
+  
+  }
+}
